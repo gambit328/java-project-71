c41f9cf40c32d38d29d9bb84b3d29d777b21cb03
gambit328 <darklar@mail.ru>
2025-08-26 15:39:14 +0700

    prepared project

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    checkstyle
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("io.freefair.lombok") version "8.14.2"
+    id("org.sonarqube") version "6.3.0.5676"
+
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+    implementation("org.apache.commons:commons-collections4:4.5.0")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "gambit328_java-project-71")
+        property("sonar.projectName", "gambit328-1")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
