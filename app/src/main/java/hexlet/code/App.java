5ae483669cf26678e12320e4fc3c028738991c83
gambit328 <darklar@mail.ru>
2025-08-27 14:25:27 +0700

    added picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true
+)
+
+public class App implements Runnable {
+
+    @Override
+    public void run() {
         System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
     }
 }
