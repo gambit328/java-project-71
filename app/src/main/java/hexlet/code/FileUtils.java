b2fdcf0468b9e702776ca064375b46966a729a52
gambit328 <darklar@mail.ru>
2025-08-31 15:54:37 +0700

    fixed input user path

--- app/src/main/java/hexlet/code/FileUtils.java
+++ app/src/main/java/hexlet/code/FileUtils.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,7 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FileUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final Path FIXTURES = Path.of("src/test/resources/fixtures");
 
     private static Path getPath(Path filePath) {
@@ -22,6 +21,6 @@ public class FileUtils {
     public static Map<String, Object> readJson(Path filePath) throws IOException {
         var normalizePath = filePath.isAbsolute() ? filePath.normalize() : getPath(filePath);
 
-        return objectMapper.readValue(normalizePath.toFile(), new TypeReference<Map<String, Object>>() {});
+        return MAPPER.readValue(normalizePath.toFile(), new TypeReference<Map<String, Object>>() { });
     }
 }
