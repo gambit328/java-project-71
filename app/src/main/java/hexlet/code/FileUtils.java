21109e98849295a2cc469bec2e13929ce0227884
gambit328 <darklar@mail.ru>
2025-08-31 13:51:07 +0700

    fixed filepaths from user

--- app/src/main/java/hexlet/code/FileUtils.java
+++ app/src/main/java/hexlet/code/FileUtils.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,9 +12,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class FileUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Path FIXTURES = Path.of("src/test/resources/fixtures");
+
+    private static Path getPath(Path filePath) {
+        Path resultPath = filePath.isAbsolute() ? filePath : FIXTURES.resolve(filePath);
+        return resultPath.toAbsolutePath().normalize();
+    }
 
     public static Map<String, Object> readJson(Path filePath) throws IOException {
-        var normalizePath = filePath.isAbsolute() ? filePath.normalize() : filePath.toAbsolutePath().normalize();
+        var normalizePath = filePath.isAbsolute() ? filePath.normalize() : getPath(filePath);
 
         return objectMapper.readValue(normalizePath.toFile(), new TypeReference<Map<String, Object>>() {});
     }
