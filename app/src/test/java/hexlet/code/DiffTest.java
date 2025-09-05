520ee74a46a8c18c01775336aaa05a4e4d830c87
gambit328 <darklar@mail.ru>
2025-09-05 21:58:47 +0700

    fixed style code errors

--- app/src/test/java/hexlet/code/DiffTest.java
+++ app/src/test/java/hexlet/code/DiffTest.java
@@ -2,83 +2,96 @@ package hexlet.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Map;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class DiffTest {
 
-  @ParameterizedTest(name = "{3}")
-  @MethodSource("cases")
-  void testGenerate(String expected, Map<String, Object> content1,
-                    Map<String, Object> content2, String testName) {
-    var actual = Diff.generate(content1, content2);
-    assertEquals(expected.stripTrailing(),
-                   actual.stripTrailing());
-  }
+    @ParameterizedTest(name = "{3}")
+    @MethodSource("cases")
+    void testGenerate(
+            String expected,
+            Map<String, Object> content1,
+            Map<String, Object> content2,
+            String testName) {
+        var actual = Diff.generate(content1, content2);
+        assertEquals(expected.stripTrailing(), actual.stripTrailing());
+    }
 
-  static Stream<Arguments> cases() {
-    return Stream.of(
-      Arguments.of(
-        """
-      {
-        - follow: false
-          host: hexlet.io
-        - proxy: 123.234.53.22
-        - timeout: 50
-        + timeout: 20
-        + verbose: true
-      }
-      """,
-      Map.of("follow", false,
-            "host", "hexlet.io",
-            "proxy", "123.234.53.22",
-            "timeout", 50),
-      Map.of("timeout", 20,
-              "verbose", true,
-              "host", "hexlet.io"),
-          "basic"),
-      Arguments.of(
-    "{\n\n}", Map.of(), Map.of(), "empty"),
-      Arguments.of(
-        """
-        {
-          - follow: false
-          - host: hexlet.io
-          - proxy: 123.234.53.22
-          - timeout: 50
-          - verbose: true
-        }
-        """,
-        Map.of("follow", false,
-              "host", "hexlet.io",
-              "proxy", "123.234.53.22",
-              "timeout", 50,
-              "verbose", true),
-        Map.of(),
-        "second map is empty"
-      ),
-      Arguments.of(
-        """
-        {
-          + follow: false
-          + host: hexlet.io
-          + proxy: 123.234.53.22
-          + timeout: 50
-          + verbose: true
-        }
-        """,
-        Map.of(),
-        Map.of("follow", false,
-              "host", "hexlet.io",
-              "proxy", "123.234.53.22",
-              "timeout", 50,
-              "verbose", true),
-        "first map is empty"
-      )
-  );
-  }
+    static Stream<Arguments> cases() {
+        return Stream.of(
+                Arguments.of(
+                        """
+                        {
+                          - follow: false
+                            host: hexlet.io
+                          - proxy: 123.234.53.22
+                          - timeout: 50
+                          + timeout: 20
+                          + verbose: true
+                        }
+                        """,
+                        Map.of(
+                                "follow",
+                                false,
+                                "host",
+                                "hexlet.io",
+                                "proxy",
+                                "123.234.53.22",
+                                "timeout",
+                                50),
+                        Map.of("timeout", 20, "verbose", true, "host", "hexlet.io"),
+                        "basic"),
+                Arguments.of("{\n\n}", Map.of(), Map.of(), "empty"),
+                Arguments.of(
+                        """
+                        {
+                          - follow: false
+                          - host: hexlet.io
+                          - proxy: 123.234.53.22
+                          - timeout: 50
+                          - verbose: true
+                        }
+                        """,
+                        Map.of(
+                                "follow",
+                                false,
+                                "host",
+                                "hexlet.io",
+                                "proxy",
+                                "123.234.53.22",
+                                "timeout",
+                                50,
+                                "verbose",
+                                true),
+                        Map.of(),
+                        "second map is empty"),
+                Arguments.of(
+                        """
+                        {
+                          + follow: false
+                          + host: hexlet.io
+                          + proxy: 123.234.53.22
+                          + timeout: 50
+                          + verbose: true
+                        }
+                        """,
+                        Map.of(),
+                        Map.of(
+                                "follow",
+                                false,
+                                "host",
+                                "hexlet.io",
+                                "proxy",
+                                "123.234.53.22",
+                                "timeout",
+                                50,
+                                "verbose",
+                                true),
+                        "first map is empty"));
+    }
 }
