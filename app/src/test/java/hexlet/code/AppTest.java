a633595100d8b2860add7e1bbe977de92044ae55
gambit328 <darklar@mail.ru>
2025-09-07 20:36:29 +0700

    added tests for App

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,47 @@
+package hexlet.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+
+public final class AppTest {
+
+    private StringWriter out;
+    private StringWriter err;
+
+    @BeforeEach
+    void beforeEach() {
+        out = new StringWriter();
+        err = new StringWriter();
+    }
+
+    @Test
+    void testCallsuccess() {
+        var expected = 0;
+        var actual = new CommandLine(new App())
+        .setOut(new PrintWriter(out, true))
+        .execute("file1.json", "file2.json");
+
+        assertFalse(out.toString().isBlank());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCallFail() {
+        var expected = 1;
+        var actual = new CommandLine(new App())
+        .setOut(new PrintWriter(out, true))
+        .execute("file3.json", "file2.json");
+
+        assertTrue(out.toString().isBlank());
+        assertEquals(expected, actual);
+    }
+}
