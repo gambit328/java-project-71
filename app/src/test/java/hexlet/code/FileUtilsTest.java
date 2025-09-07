2179ace79e61bef903a0a3f47ca7f9cbd166de58
gambit328 <darklar@mail.ru>
2025-09-07 18:05:33 +0700

    fixed code style

--- app/src/test/java/hexlet/code/FileUtilsTest.java
+++ app/src/test/java/hexlet/code/FileUtilsTest.java
@@ -19,29 +19,26 @@ public class FileUtilsTest {
     @BeforeAll
     static void beforeAll() {
         file1 = Path.of("file1.json");
-        file2 = Path.of("src", "test", "resources", "fixtures", "file2.json").toAbsolutePath();
+        file2 = Path.of("src", "test", "resources",
+                "fixtures", "file2.json").toAbsolutePath();
         file3 = Path.of("file3.json");
     }
 
     @Test
     public void testReadJson() throws IOException {
-        var expected =
-                Map.of(
-                        "host",
-                        "hexlet.io",
-                        "timeout",
-                        50,
-                        "proxy",
-                        "123.234.53.22",
-                        "follow",
-                        false);
+        var expected = Map.of("host", "hexlet.io",
+                "timeout", 50,
+                "proxy", "123.234.53.22",
+                "follow", false);
         var actual = FileUtils.readJson(file1);
         assertEquals(expected, actual);
     }
 
     @Test
     public void testReadJsonabsolutePath() throws IOException {
-        var expected = Map.of("timeout", 20, "verbose", true, "host", "hexlet.io");
+        var expected = Map.of("timeout", 20,
+                "verbose", true,
+                "host", "hexlet.io");
         var actual = FileUtils.readJson(file2);
         assertEquals(expected, actual);
     }
