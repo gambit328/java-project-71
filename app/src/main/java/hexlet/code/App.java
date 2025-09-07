9c5865ae4a3b5659fc3e2129a47a94946dba9c13
gambit328 <darklar@mail.ru>
2025-09-07 19:42:51 +0700

    swap System.out.println on commandLine().getOut() and getErr()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,8 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -11,11 +13,15 @@ import java.util.concurrent.Callable;
 
 @Command(
         name = "gendiff",
-        description = "Compares two configuration files and shows a difference.",
+        description = "Compares two configuration "
+            + "files and shows a difference.",
         mixinStandardHelpOptions = true
 )
 
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
+
+    @Spec
+    private CommandSpec spec;
 
     @Option(
             names = { "-f", "--format" },
@@ -47,15 +53,15 @@ public class App implements Callable<Integer> {
             var resultPath2 = FileUtils.readJson(filepath2);
             result += Diff.generate(resultPath1, resultPath2);
 
-            System.out.println(result);
+            spec.commandLine().getOut().println(result);
             return 0;
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            spec.commandLine().getErr().println(e.getMessage());
             return 1;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         int code = new CommandLine(new App()).execute(args);
         System.exit(code);
     }
